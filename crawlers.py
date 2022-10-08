b3d528749b5d6e61f5e9c028f22aad17e926269e
adenialzz <1160180309@qq.com>
2022-10-08 13:27:55 +0800

    implement multi-thread crawler

--- crawlers.py
+++ crawlers.py
@@ -2,6 +2,8 @@ import os
 import os.path as osp
 import requests
 import random
+import logging
+from concurrent.futures import ThreadPoolExecutor
 from lxml import etree
 
 from utils import save_url_image, get_headers
@@ -14,6 +16,8 @@ class DPCImageCrawler:
         self.dpc_base_image_url = 'https://www.dpchallenge.com/image.php?IMAGE_ID='
         self.headers = get_headers(kwargs.get('ua_file', 'misc/user_agents.txt'))
         self.sess = requests.session()
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        self.logger = logging.getLogger(__name__)
 
     def get_request_config(self):
         request_config = {
@@ -22,14 +26,27 @@ class DPCImageCrawler:
         }
         return request_config
 
-    def run(self, image_names_list):
-        for image_name in image_names_list:
-            image_id = image_name.split('.')[0]
-            image_home_url = self.dpc_base_image_url + image_id
-            resp = self.sess.get(image_home_url, **self.get_request_config())
-            tree = etree.HTML(resp.text)
-            image_url = 'https:' + tree.xpath('//*[@id="img_container"]/img[2]')[0].get('src')
-            save_url_image(image_url, self.work_dir)
+    def get_dpc_image(self, image_name):
+        image_id = image_name.split('.')[0]
+        image_home_url = self.dpc_base_image_url + image_id
+        resp = self.sess.get(image_home_url, **self.get_request_config())
+        tree = etree.HTML(resp.text)
+        image_url = 'https:' + tree.xpath('//*[@id="img_container"]/img[2]')[0].get('src')
+        save_url_image(image_url, self.work_dir)
+        self.logger.info(f"{image_url.split('/')[-1]} Saved.")
 
+    def _run_single_thread(self, image_names_list):
+        for image_name in image_names_list:
+            self.get_dpc_image(image_name)
+
+    def _run_mutlti_thread(self, image_names_list, num_threads=10):
+        with ThreadPoolExecutor(num_threads) as executor:
+            executor.map(self.get_dpc_image, image_names_list)
+
+    def run(self, image_names_list, num_threads=1):
+        if num_threads == 1:
+            self._run_single_thread(image_names_list)
+        else:
+            self._run_mutlti_thread(image_names_list, num_threads=num_threads)
 
 
